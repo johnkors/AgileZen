f68bbcd62c541c294666382a3e30b27d87964853
Tore Lervik <tore@mindre.net>
2011-05-05 16:19:43 +0200

    fix

---
+++ ZenDroid/Lib/Services/AndroidDispatcher.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Lib.Services
+{
+    class AndroidDispatcher : IDispatchOnUIThread
+    {
+        public void Invoke(Action action)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
