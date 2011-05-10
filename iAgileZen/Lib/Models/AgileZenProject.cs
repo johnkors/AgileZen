96addb28cef9f6cc423f596a84e92f10012f581b
Tore Lervik <tore@mindre.net>
2011-05-10 16:05:51 +0200

    lagt til AgileZenTask og fjernet ubrukte filer

--- iAgileZen/Lib/Models/AgileZenProject.cs
+++ iAgileZen/Lib/Models/AgileZenProject.cs
@@ -1,16 +1,17 @@
 namespace AgileZen.Lib
 {
-	public class AgileZenProject
-	{	
-		public string CreateTime {get;set;}
-		public string Description {get;set;}
-		public string Id {get;set;}
-		public string Name {get;set;}
+    public class AgileZenProject
+    {
+        public string CreateTime { get; set; }
+        public string Description { get; set; }
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public AgileZenUser Owner { get; set; }
 
         public override string ToString()
         {
             return Name;
         }
-	}
+    }
 }
 
--- iAgileZen/Lib/Models/AgileZenStory.cs
+++ iAgileZen/Lib/Models/AgileZenStory.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace AgileZen.Lib
 {
     public class AgileZenStory
@@ -6,8 +8,12 @@ namespace AgileZen.Lib
         public string Text { get; set; }
         public string Size { get; set; }
         public string Status { get; set; }
+        public string Color { get; set; }
+        public string Priority { get; set; }
         public AgileZenPhase Phase { get; set; }
+        public AgileZenUser Creator { get; set; }
         public AgileZenUser Owner { get; set; }
+        public IEnumerable<AgileZenTask> Tasks { get; set; }
 
         public override string ToString()
         {
